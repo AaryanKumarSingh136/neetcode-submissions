63e4cbb908089aee9e31d749fa5b061fc018ec99
Aaryan Kumar Singh <aaryanksingh.rbt@gmail.com>
2026-08-23 22:05:33 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack.push(ch);
+            }
+            else{
+
+                if(stack.isEmpty()) return false;
+                char t=stack.pop();
+                 if(ch==')'&& t!='(') return false;
+                 if(ch==']'&& t !='[') return false;
+                 if(ch=='}'&& t !='{') return false;
+            }
+
+        }
+        return stack.isEmpty();
+
+    }
+}
